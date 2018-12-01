92ff36171b1dfd8e9b20eee2ad246538437327f4
DESKTOP-FVSCF23\htw75 <htw750128@gmail.com>
2018-12-01 09:15:51 +0800

    #更新

---
+++ Assets/Project_ 1/Ting/GetID.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetID : MonoBehaviour {
+public string installerName;
+public string identifier;
+public string productName;
+	
+	void Start ()
+	{
+	installerName = (Application.installerName);
+	identifier = (Application.identifier);
+	productName = (Application.productName);
+	}
+	
+	
+}
