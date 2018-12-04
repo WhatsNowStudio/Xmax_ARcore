42f4369662ca1f0a8b039fdb5fdd7f2500cdb19d
DESKTOP-FVSCF23\htw75 <htw750128@gmail.com>
2018-12-04 22:53:02 +0800

    更新

---
+++ Assets/Takescreenshot.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Takescreenshot : MonoBehaviour {
+
+	// Use this for initialization
+	void screenshotandsave () {
+		StartCoroutine( TakeScreenshotAndSave() );
+	}
+	
+	private IEnumerator TakeScreenshotAndSave()
+{
+	yield return new WaitForEndOfFrame();
+
+	Texture2D ss = new Texture2D( Screen.width, Screen.height, TextureFormat.RGB24, false );
+	ss.ReadPixels( new Rect( 0, 0, Screen.width, Screen.height ), 0, 0 );
+	ss.Apply();
+
+	// Save the screenshot to Gallery/Photos
+	Debug.Log( "Permission result: " + NativeGallery.SaveImageToGallery( ss, "Christmas tree AR+", "Photo {0}.png" ) );
+	
+	// To avoid memory leaks
+	Destroy( ss );
+}
+}
